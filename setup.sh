3f18d62b56bf341bdebd114328ce74a37de10ec6
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2026-01-07 15:26:56 +0530

    feat: switch to symlinks for nix file

--- setup.sh
+++ setup.sh
@@ -52,12 +52,15 @@ then
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 	echo $(success "Homebrew installed successfully!")
 
-	cp -fr "$(pwd -P)"/nix/hosts/mac/flake.nix ~/.config/nix/
+	# cp -fr "$(pwd -P)"/nix/hosts/mac/flake.nix ~/.config/nix/
+	# symlink the flake.nix from the repo to ~/.config/nix/flake.nix
+	ln -sf "$(pwd -P)"/nix/hosts/mac/flake.nix ~/.config/nix/flake.nix
 
 	echo $(info "Run \`brew.sh\` to install Homebrew packages")
 else
 	echo $(info "Detected Linux operating system")
-	cp -f "$(pwd -P)"/nix/hosts/linux/flake.nix ~/.config/nix/
+	# cp -f "$(pwd -P)"/nix/hosts/linux/flake.nix ~/.config/nix/
+	ln -sf "$(pwd -P)"/nix/hosts/linux/flake.nix ~/.config/nix/flake.nix
 fi
 
 echo $(success "Finished copying flake.nix to ~/.config/nix")
